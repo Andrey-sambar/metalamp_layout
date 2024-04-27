e02d7949f11a10fd344d998310df0ab43339dbb5
Andreysambar <andreysambar@gmail.com>
2024-04-27 21:14:34 +0300

    added filemanager-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,19 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const FileManagerPlugin = require('filemanager-webpack-plugin');
 
 module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.join(__dirname, 'src', 'template.html'),
       filename: 'index.html',
+    }),
+    new FileManagerPlugin({
+      events: {
+        onStart: {
+          delete: ['dist'],
+        },
+      },
     }),
   ],
   devServer: {
